1e53ec3ff9cd3725b369e157e134a268c36f0afe
João Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-03-19 17:55:01 -0700

    working articles box

---
+++ js/main-article-handler.js
@@ -0,0 +1,41 @@
+$(function () {
+
+    let articleImg = $("#featured-article-img");
+    let articleTitle = $("#featured-article-title");
+    let articleSubtitle = $("#featured-article-subtitle");
+
+    let sideArticles = $(".other-article-box");
+
+    sideArticles.click(function (e) {
+
+        //remove active class from the first element (Default selection(
+        sideArticles.removeClass('active');
+
+        //add the class to this article
+        $(this).addClass('active');
+
+        let imgUrl = $(this)[0].id;
+
+
+        //change main article img
+        articleImg.attr('src',`gfx/pictures/articles/${imgUrl}.jpg`);
+
+        //change article title
+        //sidebar h1
+        let h1 = $(this).children()[0];
+        let p = $(this).children()[1];
+        articleTitle.html(h1.innerHTML);
+        articleSubtitle.html(p.innerHTML);
+
+
+
+
+
+
+
+        // console.log($(this)[0].innerText);
+
+    });
+
+
+});
